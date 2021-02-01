d7f5e0dd59e8fc65be5301e9d2288690e65a6674
Erna Hulaj <78096644+ernahulaj@users.noreply.github.com>
2021-02-02 00:27:58 +0100

    Delete Process.h

--- Process.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include<string>
-#include<vector>
-using namespace std;
-
-string procesi(vector<string> Operacionet, vector<string> Emrat_Moduleve, string teksti_fillestar, string emri_modulit1, vector<string> Lista_Inputeve);
-string procesifinal(vector<string> Operacionet, vector<string> Emrat_Moduleve, string teksti_moduluar1, string emri_modulit2, vector<string> Lista_Inputeve);
