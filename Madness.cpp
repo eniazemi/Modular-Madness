b3aa3127fade2d706383d44e79503b90ed015449
Eni Azemi <65094429+eniazemi@users.noreply.github.com>
2021-02-02 20:03:00 +0100

    Update Madness.cpp

--- Madness.cpp
+++ Madness.cpp
@@ -18,13 +18,13 @@ int perseritja = 0;
 instruksionet();
 
 while (komanda.compare("quit") != 0) {									//to make the program continue until 'quit' is typed
-	getline(cin, komanda);												//to get the full line written by the user
+	getline(cin, komanda);										//to get the full line written by the user
 
-	int nr_simobleve_te_komandes = numri_simboleve(komanda);			//to count the symbols of the first word given by the user
+	int nr_simobleve_te_komandes = numri_simboleve(komanda);		//to count the symbols of the first word given by the user
 	lloji_komandes = komanda.substr(0, nr_simobleve_te_komandes);		//to select only the first given word
 
 
-	if (lloji_komandes.compare("module") == 0) {						// when the first given word is 'module', the following words are <module name> and <operation>
+	if (lloji_komandes.compare("module") == 0) {				// when the first given word is 'module', the following words are <module name> and <operation>
 		string emri = komanda.erase(0, nr_simobleve_te_komandes + 1);	//these lines of code take the module name given by the user and store it in the vector 'Emrat_Moduleve'
 		int nr_simboleve_te_emrit = numri_simboleve(emri);
 		emri_komandes = emri.substr(0, nr_simboleve_te_emrit);
@@ -50,11 +50,11 @@ while (komanda.compare("quit") != 0) {									//to make the program continue un
 
 	else if (lloji_komandes.compare("process") == 0) {					//if the first given word is 'process', the string following it should be processed
 		teksti_fillestar = komanda.erase(0, nr_simobleve_te_komandes + 1);
-		Lista_Inputeve.push_back(teksti_fillestar);						//stores input strings in the vector 'Lista_Inputeve'
+		Lista_Inputeve.push_back(teksti_fillestar);					//stores input strings in the vector 'Lista_Inputeve'
 
 		teksti_moduluar1 = procesi(Operacionet, Emrat_Moduleve, teksti_fillestar, emri_modulit1, Lista_Inputeve);			//processes the string into the first connected module
 		teksti_final = procesifinal(Operacionet, Emrat_Moduleve, teksti_moduluar1, emri_modulit2, Lista_Inputeve);			//processes the string one more time into the second module
-		Lista_Teksteve.push_back(teksti_final);							//stores the output strings
+		Lista_Teksteve.push_back(teksti_final);						//stores the output strings
 		if (perseritja < 16) {								//limits the number of output (processed) strings to 16 times
 			for (size_t i = 0; i < Lista_Teksteve.size(); i++) {
 				cout << Lista_Teksteve[i] << " ";
@@ -62,7 +62,7 @@ while (komanda.compare("quit") != 0) {									//to make the program continue un
 			}
 			cout << endl;
 		}
-		else {															//when the 16 times are reached
+		else {												//when the 16 times are reached
 			cout << "Maximum capacity has been reached. Type 'clear' to start over :(" << endl;
 
 		}
